6ddf80f2f44cdbfa491a21e29a27bb530571e606
Johannes Nupen Theigen <johannes.theigen@gmail.com>
2025-03-07 20:47:28 +0100

    Update DeckOfCardsTest
    100 % test coverage and removed tests for generateDeck as it is a private method that is internally used and therefore not needed to test.

--- src/test/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCardsTest.java
+++ src/test/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCardsTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.function.BooleanSupplier;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
