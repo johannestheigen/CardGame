c4969d51c4e13f645239164be317d3d3ff674a0e
Johannes Nupen Theigen <johannes.theigen@gmail.com>
2025-03-08 18:53:32 +0100

    Update CardGameView v-0.0.2
    Added actions to buttons, labels and display of cards.

--- src/main/java/view/CardGameView.java
+++ src/main/java/view/CardGameView.java
@@ -1,5 +1,7 @@
 package view;
 
+import controller.CardGameController;
+import java.util.List;
 import java.util.Objects;
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -8,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
@@ -19,6 +22,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import model.CardFaceManager;
+import model.CardManager;
+import model.PlayingCard;
 
 /**
  * The CardGameView class represents the view of the card game application.
@@ -26,15 +32,19 @@ import javafx.stage.Stage;
  * and it provides a graphical user interface for the card game.
  *
  * @author Johannes Nupen Theigen
- * @version 0.0.1
+ * @version 0.0.2
  * @since 03.07.2025
  */
 public class CardGameView extends Application {
 
+  CardGameController controller; // The controller of the card game application
+  CardManager cardManager; // The card manager of the card game application
+  CardFaceManager cardFaceManager; // The card face manager of the card game application
   private Label sumLabel; // The sum of the face values of the playing cards in the hand
   private Label heartsLabel; // The number of hearts in the hand
   private Label flushLabel; // The flush status of the hand
   private Label queenLabel; // The queen of spades status of the hand
+  private HBox cardPane;
 
   /**
    * The main method of the CardGameView class used to launch the graphical user interface.
@@ -48,11 +58,15 @@ public class CardGameView extends Application {
   /**
    * The start method of the CardGameView class which is used to set up the
    * graphical user interface.
-
+   *
    * @param primaryStage The primary stage of the application
    */
   @Override
   public void start(Stage primaryStage) {
+    cardManager = new CardManager();
+    controller = new CardGameController(cardManager, this);
+    cardFaceManager = new CardFaceManager();
+
     BorderPane borderPane = new BorderPane();
 
     Image backgroundImage = new Image(Objects.requireNonNull(getClass()
@@ -83,26 +97,19 @@ public class CardGameView extends Application {
     primaryStage.show();
   }
 
-  /**
+  /*
    * The createCardPane method is used to create a pane
    * for displaying the playing cards in the hand.
-
-   * @return The card pane layout
    */
   private HBox createCardPane() {
-    HBox cardPane = new HBox(10);
+    cardPane = new HBox(10);
     cardPane.setAlignment(Pos.CENTER);
-    cardPane.setPrefSize(500, 500);
-    cardPane.setStyle("-fx-border-color: black; "
-        + "-fx-border-width: 3px; -fx-background-color: transparent;");
+    cardPane.setPrefSize(500, 200);
     return cardPane;
   }
 
-  /**
-   * The createButtonPane method is used to create a pane for
-   * displaying buttons for dealing a hand, checking the hand, and shuffling the deck.
-
-   * @return The button pane layout
+  /*
+   * The createButtonPane method is used to create a pane for displaying buttons.
    */
   private VBox createButtonPane() {
     VBox buttonPane = new VBox(20);
@@ -111,18 +118,18 @@ public class CardGameView extends Application {
     buttonPane.setPadding(new Insets(100, 50, 0, 0));
 
     Button dealHandButton = createStyledButton("Deal Hand");
-    Button checkHandButton = createStyledButton("Check Hand");
-    Button shuffleDeckButton = createStyledButton("Shuffle Deck");
+    dealHandButton.setOnAction(e -> controller.dealHand());
 
-    buttonPane.getChildren().addAll(dealHandButton, checkHandButton, shuffleDeckButton);
+    Button checkHandButton = createStyledButton("Check Hand");
+    checkHandButton.setOnAction(e -> controller.checkHand());
+
+
+    buttonPane.getChildren().addAll(dealHandButton, checkHandButton);
     return buttonPane;
   }
 
-  /**
+  /*
    * The createStyledButton method is used to create a button with a specific style.
-
-   * @param text The text of the button
-   * @return The styled button
    */
   private Button createStyledButton(String text) {
     Button button = new Button(text);
@@ -132,10 +139,8 @@ public class CardGameView extends Application {
     return button;
   }
 
-  /**
+  /*
    * The createInfoPane method is used to create a pane for displaying information about the hand.
-
-   * @return The info pane layout
    */
   private GridPane createInfoPane() {
     GridPane infoPane = new GridPane();
@@ -144,12 +149,12 @@ public class CardGameView extends Application {
     infoPane.setVgap(15);
 
     sumLabel = new Label("0");
-    heartsLabel = new Label("0");
+    heartsLabel = new Label("No");
     flushLabel = new Label("No");
     queenLabel = new Label("No");
 
-    HBox sumPane = createInfoRow("Sum of cards:", sumLabel);
-    HBox heartsPane = createInfoRow("Number of hearts:", heartsLabel);
+    HBox sumPane = createInfoRow("Sum:", sumLabel);
+    HBox heartsPane = createInfoRow("Hearts:", heartsLabel);
     HBox flushPane = createInfoRow("Flush:", flushLabel);
     HBox queenPane = createInfoRow("Queen of Spades:", queenLabel);
 
@@ -164,8 +169,8 @@ public class CardGameView extends Application {
   /**
    * The createInfoRow method is used to create a row with a label and a value label.
    * The method creates an HBox layout with a label and a value label, and returns the layout.
-
-   * @param labelText The text of the label
+   *
+   * @param labelText  The text of the label
    * @param valueLabel The value label
    * @return The info row layout
    */
@@ -178,12 +183,54 @@ public class CardGameView extends Application {
     label.setStyle("-fx-font-size: 25px; -fx-font-family:"
         + " 'Poppins'; -fx-fill: white;");
 
-    valueLabel.setPrefSize(60, 40);
+    valueLabel.setPrefSize(140, 50);
     valueLabel.setAlignment(Pos.CENTER);
     valueLabel.setStyle("-fx-font-size: 20px; -fx-font-family: 'Garamond'; "
         + " -fx-background-color: #2d6a4f; -fx-text-fill: white; -fx-border-color: black;");
 
     row.getChildren().addAll(label, valueLabel);
     return row;
+  }
+
+  /**
+   * The updateInfoPane method is used to update the information
+   * pane with the sum of the face values,
+   * the number of hearts, the flush status, and the queen of spades status of the hand.
+
+   * @param sum the sum of the face values
+   * @param hearts the number of hearts
+   * @param hasFlush the flush status
+   * @param hasQueenOfSpades the queen of spades status
+   */
+  public void updateInfoPane(int sum, List<String> hearts,
+                             boolean hasFlush, boolean hasQueenOfSpades) {
+    sumLabel.setText(String.valueOf(sum));
+    heartsLabel.setText(hearts.isEmpty() ? "No" : String.join(", ", hearts));
+    flushLabel.setText(hasFlush ? "Yes" : "No");
+    queenLabel.setText(hasQueenOfSpades ? "Yes" : "No");
+  }
+
+  /**
+   * The updateCardPane method is used to update the card pane with the playing cards in the hand.
+   */
+  public void updateCardPane() {
+    cardPane.getChildren().clear();
+
+    for (PlayingCard card : cardManager.getHand().getCardsOnHand()) {
+      Image image = cardFaceManager.getCardImage(card);
+      if (image != null) {
+        cardPane.getChildren().add(createCardImageView(image));
+      }
+    }
+  }
+
+  /*
+    * The createCardImageView method is used to create an image view of a playing card.
+   */
+  private ImageView createCardImageView(Image image) {
+    ImageView imageView = new ImageView(image);
+    imageView.setFitWidth(100);
+    imageView.setFitHeight(150);
+    return imageView;
   }
 }
