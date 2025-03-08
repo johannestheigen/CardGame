b090cb24d69990a2376ed0871333cf74db4d529d
Johannes Nupen Theigen <johannes.theigen@gmail.com>
2025-03-08 18:53:54 +0100

    Add CardGameController v-0.0.1

---
+++ src/main/java/controller/CardGameController.java
@@ -0,0 +1,58 @@
+package controller;
+
+import java.util.List;
+import model.CardManager;
+import view.CardGameView;
+
+/**
+ * The CardGameController class represents the controller of the card game application.
+ * It for interacting with the CardManager and CardGameView classes to connect
+ * logic and view together.
+ *
+ * @author Johannes Nupen Theigen
+ * @version 0.0.1
+ * @since 03.08.2025
+ */
+public class CardGameController {
+  private final CardManager cardManager;
+  private final CardGameView cardGameView;
+
+  /**
+   * Creates an instance of a CardGameController object with a card manager and a card game view.
+
+   * @param cardManager The card manager of the card game application which
+   *                    is used to manage the deck of playing cards and the hand of playing cards
+   * @param cardGameView The card game view of the card game application
+   *                     which is used to display the graphical user interface
+   */
+  public CardGameController(CardManager cardManager, CardGameView cardGameView) {
+    this.cardManager = cardManager;
+    this.cardGameView = cardGameView;
+  }
+
+  /**
+   * Draws a hand of playing cards from the deck of playing
+   * cards and adds them to the hand of playing cards,
+   * and updates the card pane in the card game view.
+   */
+  public void dealHand()  {
+    cardManager.drawCards(5);
+    cardGameView.updateCardPane();
+  }
+
+  /**
+   * Checks the hand of playing cards for the following:
+   * - The sum of the face values of the playing cards in the hand
+   * - The number of hearts in the hand
+   * - The flush status of the hand
+   * - The queen of spades status of the hand.
+   */
+  public void checkHand() {
+    int sum = cardManager.calculateSumOfCardsOnHand();
+    List<String> hearts = cardManager.getHand().getHeartCardStrings();
+    boolean hasFlush = cardManager.checkIfHandContainsFlush();
+    boolean hasQueenOfSpades = cardManager.checkIfHandContainsSpadeAndQueen();
+
+    cardGameView.updateInfoPane(sum, hearts, hasFlush, hasQueenOfSpades);
+  }
+}
